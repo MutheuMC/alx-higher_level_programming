e6edc8f4abc71498557ade0f3b048dc4235ebcbb
MutheuMC <cheldeanmutheu1@gmail.com>
2023-03-19 16:43:27 -0700

    using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!usr/bin/python3
+
+"""MySQLdb client to select all states"""
+import MySQLdb
+import sys
+
+if __name__ = "main":
+    connect_options = {
+            "user": sys.argv[1],
+            "password": sys.argv[2],
+            "database": sys.argv[3],
+            "host" : "localhost",
+            "port" : 3306
+
+
+            }
+conn = MySQLdb.connect(**connect_options)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC"
+results = cur.fetchall()
+
+for row in results:
+    print(row)
+
+cur.close()
+conn.close()
+
