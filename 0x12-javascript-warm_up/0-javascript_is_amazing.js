cdfc4af95263be2055757868c8fcce2f4009bb6e
MutheuMC <cheldeanmutheu1@gmail.com>
2023-06-13 01:55:46 +0300

    Added task one to print using console log

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//constant that prints to the console javascript is amaizing
+
+
+const myVar = 'Javascript is amazing'
+console.log(myVar)
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// use console.log to print out three lines of code
+
+console.log('C is fun')
+console.log('Python is cool')
+console.log('Javascript is amazing')
